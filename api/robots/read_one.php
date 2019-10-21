c2016ad770f27e8ceaae7c8aa5c8cb168f76870c
Roman Vainer <johonokol13@gmail.com>
2019-10-21 23:43:51 +0600

    пропустил ->bindParam

---
+++ api/robots/read_one.php
@@ -0,0 +1,29 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+include_once '../../Configuration/DataBase.php';
+include_once '../../Models/Goods.php';
+
+
+//Создаем объект из класса и коннектимся
+$database = new Database();
+$db = $database->connect();
+// Создаем объект товара
+$product = new Goods($db);
+
+// получаем id
+$product->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+// получить робота
+$product->readOneRobo();
+// созданте массива
+$good_item = array(
+    'id' => $product->id,
+    'name' => $product->name,
+    'type' => $product->type,
+    'year' => $product->year,
+    'description' => $product->description
+);
+
+// Создание JSON
+print_r(json_encode($good_item));
