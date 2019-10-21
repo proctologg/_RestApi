386defed5f985e0caccfddd8a9870b59a40e0a78
Roman Vainer <johonokol13@gmail.com>
2019-10-22 00:15:48 +0600

    описал создание робота

---
+++ api/robots/create.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../Configuration/DataBase.php';
+include_once '../../Models/Goods.php';
+
+$database = new Database();
+$db = $database->connect();
+// Создаем объект товара
+$product = new Goods($db);
+
+
+// получаем равку данны
+$data = json_decode(file_get_contents("php://input"));
+
+$product->name = $data->name;
+$product->type = $data->type;
+$product->year = $data->year;
+$product->description = $data->descripton;
+
+// Create post
+if($product->create()) {
+    echo json_encode(
+        array('trueMesssage' => 'Робот создан в базе данных')
+    );
+} else {
+    echo json_encode(
+        array('falseMessage' => 'Робот создан в базе данных')
+    );
+}
