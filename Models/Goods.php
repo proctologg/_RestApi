9649b80e81e8d94f2df0525c8a9c4391fad51e88
Roman Vainer <johonokol13@gmail.com>
2019-10-21 23:26:35 +0600

    описал метод для получения одного робота

--- Models/Goods.php
+++ Models/Goods.php
@@ -1,4 +1,5 @@
 <?php
+
 class Goods
 {
     private $conn;
@@ -16,15 +17,35 @@ class Goods
     }
 
 // Получить всех роботов
-public function readRobo()
- {
-     //создаем запрос
-     $query = 'SELECT id, name, type, year, description FROM ' . $this->table . '';
-     // подготавливаем
-     $statement = $this->conn->prepare($query);
-     //выполняем запрос
-     $statement->execute();
-     return $statement;
- }
+    public function readRobo()
+    {
+        //создаем запрос
+        $query = 'SELECT id, name, type, year, description FROM ' . $this->table . '';
+        // подготавливаем
+        $statement = $this->conn->prepare($query);
+        //выполняем запрос
+        $statement->execute();
+        return $statement;
+    }
 
+    //Получить одного робота
+    public function readOneRobo()
+    {
+        $query = 'SELECT id, name, type, year, description FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
+        // подготавливаем
+        $statement = $this->conn->prepare($query);
+        //Биндим id
+        ;$statement(1, $this->id);
+        //выполняем запрос
+        $statement->execute();
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+        // устанавливаем свойства
+        $this->name = $row['name'];
+        $this->type = $row['type'];
+        $this->year = $row['year'];
+        $this->description = $row['description'];
+
+    }
 }
